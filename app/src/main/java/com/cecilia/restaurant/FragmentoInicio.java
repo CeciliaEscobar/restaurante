961a0ede31aeceaad787eed95c206c1b3022d9bd
CeciliaEscobar <escobarceciramoa@gmail.com>
2023-10-01 11:16:56 -0300

    Finalizacion pantalla MENU y EVENTOS

--- app/src/main/java/com/cecilia/restaurant/FragmentoInicio.java
+++ app/src/main/java/com/cecilia/restaurant/FragmentoInicio.java
@@ -4,13 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class FragmentoInicio extends Fragment {
+
+    View view;
+    Button botonInicioReservar;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragmento_inicio, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragmento_inicio, container, false);
+        botonInicioReservar = (Button) view.findViewById(R.id.btnreservar);
+        botonInicioReservar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Reservando mesa...", Toast.LENGTH_LONG).show();
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/cecilia/restaurant/FragmentoMenu.java
+++ app/src/main/java/com/cecilia/restaurant/FragmentoMenu.java
@@ -1,16 +1,51 @@
 package com.cecilia.restaurant;
 
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.Fragment;
 
 public class FragmentoMenu extends Fragment {
+
+    View view;
+    Button botonMenuUno;
+    Button botonMenuDos;
+    Button botonMenuTres;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragmento_menu, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragmento_menu, container, false);
+
+        botonMenuUno = (Button) view.findViewById(R.id.btnmenu1);
+        botonMenuUno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Filete de Salmón Braseado... agregado a Favoritos", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        Boton Dos
+        botonMenuDos = (Button) view.findViewById(R.id.btnmenu3);
+        botonMenuDos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Filet Mignon al Romero... agregado a Favoritos", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        Boton Tres
+        botonMenuTres = (Button) view.findViewById(R.id.btnmenu3);
+        botonMenuTres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Mousse de Chocolate con Frutas de Verano... agregado a Favoritos", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 }
